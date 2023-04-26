3efc830eb3c7ce522788fe1dc1035114ce1ba480
anuh7 <reddyanuhya3@gmail.com>
2023-04-26 23:44:26 +0000

    added socket as package

--- base_external/package/socket/socket.mk
+++ base_external/package/socket/socket.mk
@@ -1,27 +1,28 @@
 ##############################################################
 #
-# SOCKET-SERVER
+# socket
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contentsT
-SERVER_VERSION = 'efbc6118ae9c6b7b995abad5c3c0e035f8b5da59'
+
+SOCKET_VERSION = 'c2a8362e25d126c32575ea0b4dd0a53ff4d501a2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:T
-SERVER_SITE = 'git@github.com:anuh7/final-project-assignment-anuh.git'
-SERVER_SITE_METHOD = git
-SERVER_GIT_SUBMODULES = YES
+# Your site should start with git@github.com:
+SOCKET_SITE = 'git@github.com:anuh7/final-project-assignment-anuh.git'
+SOCKET_SITE_METHOD = git
+SOCKET_GIT_SUBMODULES = YES
 
-define SERVER_BUILD_CMDS
+define SOCKET_BUILD_CMDS
         $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/client all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define SERVER_INSTALL_TARGET_CMDS
-        
+define SOCKET_INSTALL_TARGET_CMDS
         $(INSTALL) -m 0755 $(@D)/server/server $(TARGET_DIR)/usr/bin
-        
+	$(INSTALL) -m 0755 $(@D)/client/client $(TARGET_DIR)/usr/bin/
 endef
+
 
 $(eval $(generic-package))
